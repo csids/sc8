7048bdb12237099c289fddb35450a1cb5db59e28
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-03-29 08:04:08 +0200

    dispatch

--- R/v8_collect_sc_tbl.R
+++ R/v8_collect_sc_tbl.R
@@ -1,11 +1,11 @@
 #' collect
 #' @param x tbl object
+#' @param ... dots
 #' @method collect sc_tbl_v8
 #' @export
 #' @importFrom dplyr collect
-collect.sc_tbl_v8 <- function(x, order_by_keys = TRUE){
-  retval <- retval %>%
-    dplyr::collect()
+collect.sc_tbl_v8 <- function(x, ...){
+  collect(NextMethod())
 }
 
 
